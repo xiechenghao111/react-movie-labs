eb049fa717dd7c2f1b0deb8d9cf1c71d3a2aa013
chenghao xie <1152759491@qq.com>
2022-10-24 18:36:41 +0800

    Updating movie page to use react query

--- movies/src/api/tmdb-api.js
+++ movies/src/api/tmdb-api.js
@@ -12,11 +12,22 @@ export const getMovies = () => {
   });
 };
   
-  export const getMovie = id => {
-    return fetch(
-      `https://api.themoviedb.org/3/movie/${id}?api_key=${process.env.REACT_APP_TMDB_KEY}`
-    ).then(res => res.json());
-  };
+export const getMovie = (args) => {
+  // console.log(args)
+  const [, idPart] = args.queryKey;
+  const { id } = idPart;
+  return fetch(
+    `https://api.themoviedb.org/3/movie/${id}?api_key=${process.env.REACT_APP_TMDB_KEY}`
+  ).then((response) => {
+    if (!response.ok) {
+      throw new Error(response.json().message);
+    }
+    return response.json();
+  })
+  .catch((error) => {
+    throw error
+ });
+};
   export const getupcomingMovie = () => {
     return fetch(
       `https://api.themoviedb.org/3/movie/upcoming?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&page=1`
@@ -41,12 +52,21 @@ export const getMovies = () => {
    });
   };
   
-  export const getMovieImages = (id) => {
+  export const getMovieImages = ({ queryKey }) => {
+    const [, idPart] = queryKey;
+    const { id } = idPart;
     return fetch(
       `https://api.themoviedb.org/3/movie/${id}/images?api_key=${process.env.REACT_APP_TMDB_KEY}`
-    )
-      .then((res) => res.json())
-      .then((json) => json.posters);
+    ).then( (response) => {
+      if (!response.ok) {
+        throw new Error(response.json().message);
+      }
+      return response.json();
+  
+    })
+    .catch((error) => {
+      throw error
+   });
   };
   export const getMovieReviews = (id) => {
     return fetch(
