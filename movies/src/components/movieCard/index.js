d7a9f124fe312d69f443db1526e90405d99e6c65
chenghao xie <1152759491@qq.com>
2022-10-25 21:04:57 +0800

    CI - Basic pipeline.

--- movies/src/components/movieCard/index.js
+++ movies/src/components/movieCard/index.js
@@ -16,7 +16,7 @@ import { Link } from "react-router-dom";
 import Avatar from '@mui/material/Avatar';
 import { MoviesContext } from "../../contexts/moviesContext";
 export default function MovieCard({ movie, action }) {
-  const { favorites, addToFavorites } = useContext(MoviesContext);
+  const { favorites } = useContext(MoviesContext);
 
   if (favorites.find((id) => id === movie.id)) {
     movie.favorite = true;
