3dee5d4d86a4f20b205e99acbc9c9998a1b5bea3
chenghao xie <1152759491@qq.com>
2022-11-08 13:56:13 +0000

    init

--- movies/movies/src/util.js
+++
@@ -1,8 +0,0 @@
-import truncate from "lodash/truncate";
-
-export function excerpt(string) {
-  return truncate(string, {    
-    length: 400, // maximum 400 characters
-    separator: /,?\.* +/, // separate by spaces, including preceding commas and periods
-  });
-}
